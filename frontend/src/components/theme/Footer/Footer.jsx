5b9f50fad4ecaf391e59520eb2a1776472cdb24f
nzambello <nzambello@protonmail.com>
2023-06-22 18:05:47 +0200

    refactor: enforce colors on scroll, fix safari theme color with explicit body bg

--- frontend/src/components/theme/Footer/Footer.jsx
+++ frontend/src/components/theme/Footer/Footer.jsx
@@ -7,7 +7,9 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { injectIntl } from 'react-intl';
 import FooterColumns from '@package/components/theme/Footer/FooterColumns';
-import { InView } from 'react-intersection-observer';
+import useInView from '@package/helpers/useInView';
+import { BodyClass } from '@plone/volto/helpers';
+import { Helmet } from '@plone/volto/helpers';
 
 /**
  * Component to display the footer.
@@ -15,49 +17,53 @@ import { InView } from 'react-intersection-observer';
  * @param {Object} intl Intl object
  * @returns {string} Markup of the component
  */
-const Footer = ({ intl }) => (
-  <footer className="site-footer" role="contentinfo">
-    <InView
-      as="div"
-      className="header-visibility-sensor"
-      onChange={(inView, entry) => {
-        if (inView && document.body.scrollHeight > 1200) {
-          document.body.classList.add('footer-in-view');
-        } else if (document.body.classList.contains('footer-in-view')) {
-          document.body.classList.remove('footer-in-view');
-        }
-      }}
-    >
-      {' '}
-    </InView>
+const Footer = ({ intl }) => {
+  const footerInView = useInView(
+    'footer#site-footer',
+    {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    },
+    true,
+  );
 
-    <Container>
-      <FooterColumns />
-      <div className="sponsors-wrapper">
-        <div className="sponsor">
-          <a
-            aria-label="ANBI"
-            href="https://anbi.nl/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span class="sponsors-logo anbi"></span>
-          </a>
+  return (
+    <footer id="site-footer" role="contentinfo">
+      {footerInView && (
+        <>
+          <BodyClass className="footer-in-view" />
+        </>
+      )}
+
+      <Container>
+        <FooterColumns />
+        <div className="sponsors-wrapper">
+          <div className="sponsor">
+            <a
+              aria-label="ANBI"
+              href="https://anbi.nl/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span class="sponsors-logo anbi"></span>
+            </a>
+          </div>
+          <div className="sponsor">
+            <a
+              aria-label="OCW"
+              href="https://www.denhaag.nl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span class="sponsors-logo ocw"></span>
+            </a>
+          </div>
         </div>
-        <div className="sponsor">
-          <a
-            aria-label="OCW"
-            href="https://www.denhaag.nl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span class="sponsors-logo ocw"></span>
-          </a>
-        </div>
-      </div>
-    </Container>
-  </footer>
-);
+      </Container>
+    </footer>
+  );
+};
 
 /**
  * Property types.

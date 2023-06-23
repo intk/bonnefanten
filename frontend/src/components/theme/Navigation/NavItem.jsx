21de73db15c0a5bd9b89af961fb07dc0a0e0d166
nzambello <nzambello@protonmail.com>
2023-06-23 09:17:00 +0200

    feat: header / nav menu, fix selection and non content padding

--- frontend/src/components/theme/Navigation/NavItem.jsx
+++ frontend/src/components/theme/Navigation/NavItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { isInternalURL } from '@plone/volto/helpers';
 import config from '@plone/volto/registry';
@@ -6,24 +6,37 @@ import cx from 'classnames';
 
 const NavItem = ({ item, lang, onClose, level = 0 }) => {
   const { settings } = config;
+  const [open, setOpen] = useState(false);
+
   // The item.url in the root is ''
   // TODO: make more consistent it everywhere (eg. reducers to return '/' instead of '')
   if (isInternalURL(item.url) || item.url === '') {
     return (
       <li className={cx('item', `level-${level}`, item.review_state)}>
-        <NavLink
-          to={item.url === '' ? '/' : item.url}
-          key={item.url}
-          activeClassName="active"
-          exact={
-            settings.isMultilingual ? item.url === `/${lang}` : item.url === ''
-          }
-          onClick={onClose}
-        >
-          <span>{item.title}</span>
-        </NavLink>
-        {!!item.items?.length && (
-          <ul className="nav-items nav-items-nested">
+        {!!item.items?.length && level === 0 ? (
+          <button
+            className={cx('item-link', { open })}
+            onClick={() => setOpen(!open)}
+          >
+            {item.title}
+          </button>
+        ) : (
+          <NavLink
+            to={item.url === '' ? '/' : item.url}
+            key={item.url}
+            activeClassName="active"
+            exact={
+              settings.isMultilingual
+                ? item.url === `/${lang}`
+                : item.url === ''
+            }
+            onClick={onClose}
+          >
+            <span>{item.title}</span>
+          </NavLink>
+        )}
+        {!!item.items?.length && level === 0 && open && (
+          <ul className="nav-items-nested">
             {item.items.map((child) => (
               <NavItem
                 item={child}

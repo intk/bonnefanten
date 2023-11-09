5235518ea8245e495d6c3d0a99c03c7a0c377b82
Cihan Andac <cihanandac@gmail.com>
2023-11-09 15:19:09 +0100

    Add pagination icons

---
+++ frontend/src/customizations/components/manage/Blocks/Search/ListingBody.jsx
@@ -0,0 +1,141 @@
+import React, { createRef } from 'react';
+import { FormattedMessage, injectIntl } from 'react-intl';
+import cx from 'classnames';
+import { Pagination, Dimmer, Loader } from 'semantic-ui-react';
+import config from '@plone/volto/registry';
+// eslint-disable-next-line import/no-unresolved
+import withQuerystringResults from './withQuerystringResults';
+
+import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
+
+const ListingBody = withQuerystringResults((props) => {
+  const {
+    data = {},
+    isEditMode,
+    listingItems,
+    totalPages,
+    onPaginationChange,
+    variation,
+    currentPage,
+    prevBatch,
+    nextBatch,
+    isFolderContentsListing,
+    hasLoaded,
+  } = props;
+
+  let ListingBodyTemplate;
+  // Legacy support if template is present
+  const variations = config.blocks?.blocksConfig['listing']?.variations || [];
+  const defaultVariation = variations.filter((item) => item.isDefault)?.[0];
+
+  if (data.template && !data.variation) {
+    const legacyTemplateConfig = variations.find(
+      (item) => item.id === data.template,
+    );
+    ListingBodyTemplate = legacyTemplateConfig.template;
+  } else {
+    ListingBodyTemplate =
+      variation?.template ?? defaultVariation?.template ?? null;
+  }
+
+  const listingRef = createRef();
+
+  const NoResults = variation?.noResultsComponent
+    ? variation.noResultsComponent
+    : config.blocks?.blocksConfig['listing'].noResultsComponent;
+
+  const HeadlineTag = data.headlineTag || 'h2';
+
+  return (
+    <>
+      {data.headline && (
+        <HeadlineTag
+          className={cx('headline', {
+            emptyListing: !listingItems?.length > 0,
+          })}
+        >
+          {data.headline}
+        </HeadlineTag>
+      )}
+      {listingItems?.length > 0 ? (
+        <div ref={listingRef}>
+          <ListingBodyTemplate
+            items={listingItems}
+            isEditMode={isEditMode}
+            {...data}
+            {...variation}
+          />
+          {totalPages > 1 && (
+            <div className="pagination-wrapper">
+              <Pagination
+                activePage={currentPage}
+                totalPages={totalPages}
+                onPageChange={(e, { activePage }) => {
+                  !isEditMode &&
+                    listingRef.current.scrollIntoView({ behavior: 'smooth' });
+                  onPaginationChange(e, { activePage });
+                }}
+                firstItem={null}
+                lastItem={null}
+                prevItem={{
+                  content: (
+                    <BsArrowLeft
+                      icon
+                      className="leftarrow"
+                      aria-label="left arrow"
+                    ></BsArrowLeft>
+                  ),
+                  icon: true,
+                  'aria-disabled': !prevBatch,
+                  className: !prevBatch ? 'disabled' : null,
+                }}
+                nextItem={{
+                  content: (
+                    <BsArrowRight
+                      icon
+                      className="leftarrow"
+                      aria-label="left arrow"
+                    ></BsArrowRight>
+                  ),
+                  icon: true,
+                  'aria-disabled': !nextBatch,
+                  className: !nextBatch ? 'disabled' : null,
+                }}
+              />
+            </div>
+          )}
+        </div>
+      ) : isEditMode ? (
+        <div className="listing message" ref={listingRef}>
+          {isFolderContentsListing && (
+            <FormattedMessage
+              id="No items found in this container."
+              defaultMessage="No items found in this container."
+            />
+          )}
+          {hasLoaded && NoResults && (
+            <NoResults isEditMode={isEditMode} {...data} />
+          )}
+          <Dimmer active={!hasLoaded} inverted>
+            <Loader indeterminate size="small">
+              <FormattedMessage id="loading" defaultMessage="Loading" />
+            </Loader>
+          </Dimmer>
+        </div>
+      ) : (
+        <div className="emptyListing">
+          {hasLoaded && NoResults && (
+            <NoResults isEditMode={isEditMode} {...data} />
+          )}
+          <Dimmer active={!hasLoaded} inverted>
+            <Loader indeterminate size="small">
+              <FormattedMessage id="loading" defaultMessage="Loading" />
+            </Loader>
+          </Dimmer>
+        </div>
+      )}
+    </>
+  );
+});
+
+export default injectIntl(ListingBody);

05efa80cab2cd17b0235db99f802a89d3aef40da
Cihan Andac <cihanandac@gmail.com>
2023-11-27 15:07:42 +0100

    Fix input is undefined error

--- frontend/src/components/blocks/Listing/TruncateText.jsx
+++ frontend/src/components/blocks/Listing/TruncateText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class TruncateText extends React.Component {
   truncate(input) {
-    if (input?.length > 98) {
+    if (input !== undefined && input !== null && input.length > 98) {
       return input.substring(0, 98) + '...';
     }
     return input;

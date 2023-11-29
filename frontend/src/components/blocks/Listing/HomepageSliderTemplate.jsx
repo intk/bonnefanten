4437ea272b9758f5ad3de49259c158414ceb9166
Cihan Andac <cihanandac@gmail.com>
2023-11-29 14:17:56 +0100

    Changed the theme colors of the homepage

--- frontend/src/components/blocks/Listing/HomepageSliderTemplate.jsx
+++ frontend/src/components/blocks/Listing/HomepageSliderTemplate.jsx
@@ -3,15 +3,29 @@ import PropTypes from 'prop-types';
 // import { ListingBlockHeader } from '@package/components';
 import { UniversalLink } from '@plone/volto/components';
 import ArtworkPreview from '../../theme/ArtworkPreview/ArtworkPreview';
-// import { BodyClass } from '@plone/volto/helpers';
+import { BodyClass } from '@plone/volto/helpers';
 import ReactSwipe from 'react-swipe';
 import './less/HomepageSliderTemplate.less';
 import { When } from '@package/customizations/components/theme/View/EventDatesInfo';
+import useInView from '@package/helpers/useInView';
 
 const Card = ({ item, showDescription = true }) => {
+  const titleInView = useInView(
+    'p',
+    {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    },
+    true,
+  );
   return (
     <div className="plone-item-card">
-      {/* <BodyClass className="masonary-listing-page" /> */}
+      {titleInView ? (
+        <BodyClass className="homepage-title-in-view" />
+      ) : (
+        <BodyClass className="homepage-title-out-of-view" />
+      )}
       <UniversalLink href={item['@id']} className="plone-item-card-link">
         <div className="content">
           <ArtworkPreview {...item} />
@@ -94,7 +108,7 @@ const HomepageSliderTemplate = (props) => {
               >
                 <path
                   d="M0,34.7584,0,42.5l0,7.7417L34.59,85H50V69.5158L23.1147,42.5,50,15.4843V0H34.59ZM36.5976,80.2423,6.6616,50.1609l.43-.0046,6.8586.071L43.74,80.1616Zm8.6675-8.71-.08,7.1768L16.1323,49.5163l3.6114-3.6288Zm0-58.0655L19.7437,39.1126l-3.6114-3.6288L45.1847,6.2905ZM43.74,4.8385,13.95,34.7728l-6.8586.071-.43-.0047L36.5976,4.7578Z"
-                  fill="#cfb3e4"
+                  fill=""
                 />
               </svg>
             </button>
@@ -118,7 +132,7 @@ const HomepageSliderTemplate = (props) => {
                 <title>Bonnefanten_Website_Assets</title>
                 <path
                   d="M50,50.2417,50,42.5l0-7.7417L15.41,0H0V15.4842L26.8853,42.5,0,69.5158V85H15.41ZM13.4024,4.7577l29.936,30.0814-.43.0046-6.8587-.071L6.26,4.8385Zm-8.6675,8.71.08-7.1769L33.8677,35.4837l-3.6114,3.6288Zm0,58.0654L30.2563,45.8875l3.6114,3.6288L4.8152,78.71ZM6.26,80.1615,36.05,50.2273l6.8587-.071.43.0046L13.4024,80.2423Z"
-                  fill="#cfb3e4"
+                  fill=""
                 />
               </svg>
             </button>

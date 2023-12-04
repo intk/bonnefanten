e2f1002c61f5f2cbf91e2250f44f7cc15eb853a6
Cihan Andac <cihanandac@gmail.com>
2023-12-04 15:38:25 +0100

    Fix preview image

--- frontend/src/customizations/components/theme/PreviewImage/PreviewImage.jsx
+++ frontend/src/customizations/components/theme/PreviewImage/PreviewImage.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { flattenToAppURL } from '@plone/volto/helpers';
-import config from '@plone/volto/registry';
+// import config from '@plone/volto/registry';
 
-import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
+// import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
 
 /**
  * Renders a preview image for a catalog brain result item.
  *
  */
 function PreviewImage(props) {
-  const item = props;
+  const { item } = props;
 
   const src = item.image_field
     ? flattenToAppURL(`${item['@id']}/@@images/${item.image_field}`)
-    : config.getComponent({
-        name: 'DefaultImage',
-        dependencies: ['listing', 'summary'],
-      }).component || DefaultImageSVG;
+    : '';
 
   return <img src={src} alt={'alt' ?? item.title} />;
 }

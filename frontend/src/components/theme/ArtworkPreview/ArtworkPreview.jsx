157ec6458e1544f122f602c800fe7f364ca8d7eb
Cihan Andac <cihanandac@gmail.com>
2023-12-05 11:08:43 +0100

    Fix for preview images

--- frontend/src/components/theme/ArtworkPreview/ArtworkPreview.jsx
+++ frontend/src/components/theme/ArtworkPreview/ArtworkPreview.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { flattenToAppURL } from '@plone/volto/helpers';
-import config from '@plone/volto/registry';
+// import config from '@plone/volto/registry';
 import { Link } from 'react-router-dom';
 
-import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
+// import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
 
 /**
  * Renders a preview image for a catalog brain result item.
@@ -15,13 +15,8 @@ function ArtworkPreview(props) {
   const item = props;
 
   const src = item.image_field
-    ? flattenToAppURL(
-        `${item['@id']}/${item.image_scales?.preview_image[0]?.scales?.preview.download}`,
-      )
-    : config.getComponent({
-        name: 'DefaultImage',
-        dependencies: ['listing', 'summary'],
-      }).component || DefaultImageSVG;
+    ? flattenToAppURL(`${item['@id']}/@@images/${item.image_field}/preview`)
+    : '';
 
   return (
     <>

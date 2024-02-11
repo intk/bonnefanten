fcf330d8761e290bc180f5f707e2727ba2893238
Cihan Andac <cihanandac@gmail.com>
2024-02-11 21:57:50 +0100

    Changed the url for the PDFs  to open in a new tab

--- frontend/src/components/blocks/Button/ButtonView.jsx
+++ frontend/src/components/blocks/Button/ButtonView.jsx
@@ -9,12 +9,21 @@ const ButtonView = ({ data, mode = 'view' }) => {
   const isEditMode = mode === 'edit';
   const classNames = cx('text-button btn-block', data.btnStyle || 'primary');
 
+  // Check if the URL ends with "/file", indicating a potential PDF link
+  const hasDownloadPath = href.includes('/@@download/file');
+  // Modify href to replace "/@@download/file" with "/@@display-file/file" if it exists
+  const modifiedHref = hasDownloadPath
+    ? `${flattenToAppURL(
+        href.replace('/@@download/file', '/@@display-file/file'),
+      )}`
+    : flattenToAppURL(href);
+
   return isInternalURL(href) ? (
     isEditMode ? (
       <div className={classNames}>{data.linkTitle || href}</div>
     ) : (
       <ConditionalLink
-        to={flattenToAppURL(href)}
+        to={modifiedHref}
         condition={!isEditMode}
         className={classNames}
       >
@@ -22,7 +31,8 @@ const ButtonView = ({ data, mode = 'view' }) => {
       </ConditionalLink>
     )
   ) : href ? (
-    <UniversalLink href={href} className={classNames}>
+    // Ensure external links and modified document links open in a new tab
+    <UniversalLink href={modifiedHref} className={classNames} target="_blank">
       {data.linkTitle || href}
     </UniversalLink>
   ) : isEditMode ? (

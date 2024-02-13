080039e78f9467e4e457ee934a8755c3c5bf8676
Cihan Andac <cihanandac@gmail.com>
2024-02-13 14:26:13 +0100

    Changed the slideshow listing to load the preview images first and then huge versions

--- frontend/src/components/theme/SlideshowPreview/SlideshowPreview.jsx
+++ frontend/src/components/theme/SlideshowPreview/SlideshowPreview.jsx
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
 import { flattenToAppURL } from '@plone/volto/helpers';
-// import config from '@plone/volto/registry';
 import { Link } from 'react-router-dom';
 
-// import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
-
-/**
- * Renders a preview image for a catalog brain result item.
- *
- */
 function SlideshowPreview(props) {
   const item = props;
   const lazy = props.lazy;
 
-  const loadingAttribute = lazy ? 'lazy' : 'eager';
+  const [imageSrc, setImageSrc] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-  const src = item.image_field
-    ? flattenToAppURL(`${item['@id']}/@@images/${item.image_field}/huge`)
-    : '';
+  useEffect(() => {
+    // Define the base URL for the image
+    const baseUrl = item.image_field
+      ? flattenToAppURL(`${item['@id']}/@@images/${item.image_field}`)
+      : '';
+
+    // Start with low-res image
+    const lowResSrc = `${baseUrl}/preview`;
+    setImageSrc(lowResSrc); // Initially set to low-res
+
+    // Preload high-res image
+    const highResSrc = `${baseUrl}/huge`;
+    const img = new Image();
+    img.src = highResSrc;
+    img.onload = () => {
+      setImageSrc(highResSrc); // Switch to high-res once loaded
+      setIsLoading(false);
+    };
+  }, [item, item.image_field]);
+
+  const loadingAttribute = lazy ? 'lazy' : 'eager';
 
   return (
     <>
       {item.image_field !== '' ? (
         <Link to={flattenToAppURL(item['@id'])} className="listing-image">
-          <img loading={loadingAttribute} src={src} alt={item.title ?? 'alt'} />
+          <img
+            loading={loadingAttribute}
+            src={imageSrc}
+            alt={item.title ?? 'Image'}
+          />
         </Link>
       ) : (
+        // Optionally handle the case where there is no image field defined
         <img
-          src={src}
-          alt={item.title ?? 'alt'}
-          style={{ visibility: 'hidden' }}
+          src={imageSrc}
+          alt={item.title ?? 'Image'}
+          style={{ visibility: isLoading ? 'hidden' : 'visible' }}
         />
       )}
     </>
@@ -39,12 +55,12 @@ function SlideshowPreview(props) {
 }
 
 SlideshowPreview.propTypes = {
-  size: PropTypes.string,
   item: PropTypes.shape({
     '@id': PropTypes.string.isRequired,
     image_field: PropTypes.string,
     title: PropTypes.string.isRequired,
   }),
+  lazy: PropTypes.bool,
 };
 
 export default SlideshowPreview;
